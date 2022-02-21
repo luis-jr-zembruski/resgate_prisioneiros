3f72b0b7187b7187ed69afba4f972e381df1b5d2
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2022-02-21 14:37:25 -0300

    deixando loop do fundo apenas depois que iniciar o jogo

--- js/js.js
+++ js/js.js
@@ -7,23 +7,23 @@ function start() {
   $('#fundoGame').append("<div id='inimigo1' class='anima2'></div>")
   $('#fundoGame').append("<div id='inimigo2'></div>")
   $('#fundoGame').append("<div id='amigo' class='anima3'></div>")
+
+  //Principais variáveis do jogo
+
+  var jogo = {}
+
+  //Game Loop
+
+  jogo.timer = setInterval(loop, 30)
+
+  function loop() {
+    movefundo()
+  }
+
+  //Função que movimenta o fundo do jogo
+
+  function movefundo() {
+    esquerda = parseInt($('#fundoGame').css('background-position'))
+    $('#fundoGame').css('background-position', esquerda - 1)
+  }
 } // Fim da função start
-
-//Principais variáveis do jogo
-
-var jogo = {}
-
-//Game Loop
-
-jogo.timer = setInterval(loop, 30)
-
-function loop() {
-  movefundo()
-}
-
-//Função que movimenta o fundo do jogo
-
-function movefundo() {
-  esquerda = parseInt($('#fundoGame').css('background-position'))
-  $('#fundoGame').css('background-position', esquerda - 1)
-}
