f2fb8162ea5f53e052e5c9c8fb8b2d922c0fad80
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2022-02-21 17:21:18 -0300

    aumento de dificuldade

--- js/js.js
+++ js/js.js
@@ -219,6 +219,7 @@ function start() {
 
     if (colisao3.length > 0) {
       pontos = pontos + 100
+      velocidade = velocidade + 0.3
       inimigo1X = parseInt($('#inimigo1').css('left'))
       inimigo1Y = parseInt($('#inimigo1').css('top'))
 
