a1559ddef744c0c3a82a3229081b2ae7b29aa895
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2022-02-21 15:15:46 -0300

    limitação de movimentação do elicoptero

--- js/js.js
+++ js/js.js
@@ -52,11 +52,17 @@ function start() {
     if (jogo.pressionou[TECLA.W]) {
       var topo = parseInt($('#jogador').css('top'))
       $('#jogador').css('top', topo - 10)
+      if (topo <= 0) {
+        $('#jogador').css('top', topo + 10)
+      }
     }
 
     if (jogo.pressionou[TECLA.S]) {
       var topo = parseInt($('#jogador').css('top'))
       $('#jogador').css('top', topo + 10)
+      if (topo >= 434) {
+        $('#jogador').css('top', topo - 10)
+      }
     }
 
     if (jogo.pressionou[TECLA.D]) {
