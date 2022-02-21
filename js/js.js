7b5bfadb865920bb6b04126b328f583542ef9b62
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2022-02-21 14:36:18 -0300

    adição do loop da imagem de fundo do jogo

--- js/js.js
+++ js/js.js
@@ -8,3 +8,22 @@ function start() {
   $('#fundoGame').append("<div id='inimigo2'></div>")
   $('#fundoGame').append("<div id='amigo' class='anima3'></div>")
 } // Fim da função start
+
+//Principais variáveis do jogo
+
+var jogo = {}
+
+//Game Loop
+
+jogo.timer = setInterval(loop, 30)
+
+function loop() {
+  movefundo()
+}
+
+//Função que movimenta o fundo do jogo
+
+function movefundo() {
+  esquerda = parseInt($('#fundoGame').css('background-position'))
+  $('#fundoGame').css('background-position', esquerda - 1)
+}
